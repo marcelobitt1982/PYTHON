e85af16201fccbeb1c512852e022907570dbf095
Marcelo Bittencourt <142984146+marcelobitt1982@users.noreply.github.com>
2024-02-20 19:33:39 +0100

    python

---
+++ curso python/aula02/app.py
@@ -0,0 +1,13 @@
+nome = "Marcelo" String
+idade = 42 int
+altura = 1.81 float
+is_male = True / False bool
+gosto_de = "programar"
+
+
+print (f"Meu nome é {nome}")
+print (f"Eu gosto de {gosto_de}")
+print (f"Eu tenho {idade} anos")
+idade = "41"
+print (f"{nome} gosta de {gosto_de} e tem {idade} anos.")
+
---
+++ curso python/aula03/app.py
@@ -0,0 +1,35 @@
+num1 = 5
+num2 = 3.5
+
+
+
+a = float(num1)
+b = int(num2)
+
+
+
+a = int(float("5.3"))
+b = "3.5"
+
+(num1 + num2)
+(num1 - num2)
+(num1 * num2)
+(10 / 3)
+(10 // 3)
+(10 % 3)
+(2 ** 3)
+
+print((3+5)*(7+3))
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(min(1,85,17,-55,5))
+print(round(8.3))
+print(round(8.6))
+
+import math
+
+print(math.floor(8.9999))
+print(math.ceil(8.000001))
+print(math.sqrt(10))
