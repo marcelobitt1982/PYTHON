c3dc1d28a2ad95dd48f178714e7f195f94c017ea
Marcelo Bittencourt <142984146+marcelobitt1982@users.noreply.github.com>
2024-02-24 17:34:19 +0100

    input

---
+++ curso python/aula04/aula05/app.py
@@ -0,0 +1,11 @@
+#nome = input("Digite seu nome: ")
+#idade = int(input(f"Qual a sua idade {nome}?"))
+
+#nascimento = 2024 - idade
+
+#print(f"{nome} voce nasceu em {nascimentoMard}")
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+resultado = num1 + num2
+print(f"O resultado é: {resultado}")
